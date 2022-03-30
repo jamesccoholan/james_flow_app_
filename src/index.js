24f686573919e6152a5a8b684d240db3d96c0a5e
James Coholan <jamescoholan@gmail.com>
2022-03-30 16:38:39 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import Providers from "./providers/Providers.comp";
+import Routes from "./components/Routes.comp";
+import { ROUTES } from "./config/routes.config";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './components/Navbar'
-import Providers from './providers/Providers.comp';
-import Routes from './components/Routes.comp'
-import { ROUTES } from './config/routes.config';
-
-import './index.css';
-import './components/Atoms.css'
+import "./index.css";
+import "./components/Atoms.css";
 
 ReactDOM.render(
   <Providers>
     <Navbar />
     <Routes routes={ROUTES} />
   </Providers>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
