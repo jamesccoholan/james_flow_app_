101b40a5dfa07faef58c5912c9b7a8f6021bc602
James Coholan <jamescoholan@gmail.com>
2022-03-30 18:34:10 -0700

    updte

--- src/flow/list-dappy-template.script.js
+++ src/flow/list-dappy-template.script.js
@@ -2,5 +2,5 @@ export const LIST_DAPP_TEMPLATES = '
             import DappyContract from 0xDappy
             pub fun main(): {Uint32: DappyContract.Template} {
               return DappyContract.listTemplates()
-            }
-            ';
+            }'
+            
